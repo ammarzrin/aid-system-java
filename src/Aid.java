43b37f7fb9027b7dea3cb362d144c6176a7eee01
ammarzrin <ammarzrin@gmail.com>
2022-04-24 23:27:26 +0800

    aid.java and aidscompleted.java finalized and commented, working

--- src/Aid.java
+++ src/Aid.java
@@ -2,14 +2,29 @@ package src;
 
 import java.util.ArrayList;
 
+/**
+ * Aid class is a subclass under Donor which stores details for donations.
+ */
 public class Aid extends Donor {
 
     public int quantity;
     public String aid;
 
+    /**
+     * No-arg constructor
+     */
     public Aid() {
     }
 
+    /**
+     * Constructs a new Aid object that has Donor's details along with the aid and
+     * quantity.
+     * 
+     * @param name     Donor's name
+     * @param phone    Donor's phone number
+     * @param aid      Donated item
+     * @param quantity Quantity of donated item
+     */
     public Aid(String name, String phone, String aid, int quantity) {
         this.name = name;
         this.phone = phone;
@@ -17,43 +32,95 @@ public class Aid extends Donor {
         this.quantity = quantity;
     }
 
+    /**
+     * Sets the quantity of an aid object.
+     * 
+     * @param quantity Quantity of donated item
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Sets the donated item of an aid object.
+     * 
+     * @param aid Donated item
+     */
     public void setAid(String aid) {
         this.aid = aid;
     }
 
+    /**
+     * Sets the aid status to reserved when assigned to an NGO.
+     * 
+     * @return Returns a "Reserved" string to update Aid status.
+     */
     public String setReserved() {
         return "Reserved";
     }
 
+    /**
+     * Sets the aid status to collected when an NGO have collected the item.
+     * 
+     * @return Returns a "Collected" string to update Aid status.
+     */
     public String setCollected() {
         return "Collected";
     }
 
+    /**
+     * Sets the aid status to available when no NGOs have yet been assigned to the
+     * item.
+     * 
+     * @return Returns a "Available" string to update Aid status.
+     */
     public String setAvailable() {
         return "Available";
     }
 
+    /**
+     * Sets NGO and Manpower to null when an aid item is not reserved nor collected.
+     * 
+     * @return Returns a "-" string to indicate the aid's available status.
+     */
     public String setNull() {
         return "-";
     }
 
+    /**
+     * Gets aid item title.
+     * 
+     * @return Returns aid item.
+     */
     public String getAid() {
         return aid;
     }
 
+    /**
+     * Gets quantity of aid item.
+     * 
+     * @return Returns quantity of the aid item.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Default print value when printing Aid items.
+     */
     public String toString() {
         return "Donor: Name = " + getName() + ", Phone = " + getPhone() + ", Aid = " + getAid() + ", Quantity = "
                 + getQuantity();
     }
 
+    /**
+     * Compares between 2 different quantities
+     * 
+     * @param a Donor's aid item quantity
+     * @param b NGO's request item quantity
+     * @return Returns 1 if Donor's item quantity is bigger than NGO's request
+     *         quantity
+     */
     public int Compare(int a, int b) {
         if (a > b) // if quantity donor is bigger than quantity ngoName
             return 1;
@@ -63,35 +130,38 @@ public class Aid extends Donor {
             return 0; // if quantity == quantity
     }
 
+    /**
+     * Printing an object is formatted to be put into csv data files.
+     * 
+     * @return Returns a string that is used to write into a csv data file.
+     */
     public String toCSVString() {
         return getName() + "," + getPhone() + "," + getAid() + "," + getQuantity();
     }
 
+    /**
+     * Donation matching to assign Donor's donation with an NGO's item request.
+     * 
+     * @param aid             Aid object which has details of the aid item.
+     * @param requestListTemp List of requests made by NGOs
+     * @param aidCompleted    List of all donations made
+     */
     public static void matchDonation(Aid aid, ArrayList<Request> requestListTemp,
             ArrayList<AidsCompleted> aidCompleted) {
-
         for (int i = 0; requestListTemp.size() > i; i++) {
-
             int index = aid.Compare(aid.getQuantity(), requestListTemp.get(i).getQuantity());
-
             String requesting = requestListTemp.get(i).getAid();
             String aiding = aid.getAid();
-
             if (requesting.equals(aiding)) {
-
                 if (index == 1) {
-
                     aidCompleted.add(new AidsCompleted(aid.getName(), aid.getPhone(), aid.getAid(),
                             requestListTemp.get(i).getNGO(), requestListTemp.get(i).getQuantity(),
                             Integer.toString(requestListTemp.get(i).getManpower()), aid.setReserved()));
-
                     aid.setQuantity(aid.getQuantity() - requestListTemp.get(i).getQuantity());
                     requestListTemp.remove(i);
                     i--;
-
                     // code to remove the request from the list cause its finished
                 } else if (index == -1) {
-
                     aidCompleted.add(new AidsCompleted(aid.getName(), aid.getPhone(), aid.getAid(),
                             requestListTemp.get(i).getNGO(), aid.getQuantity(),
                             Integer.toString(requestListTemp.get(i).getManpower()), aid.setReserved()));
@@ -100,7 +170,6 @@ public class Aid extends Donor {
                     break;
                     // no remove request cause not done yet
                 } else {
-                    System.out.println("hoho");
                     aidCompleted.add(new AidsCompleted(aid.getName(), aid.getPhone(), aid.getAid(),
                             requestListTemp.get(i).getNGO(), aid.getQuantity(),
                             Integer.toString(requestListTemp.get(i).getManpower()), aid.setReserved()));
@@ -112,28 +181,4 @@ public class Aid extends Donor {
             }
         }
     }
-
-    // public static ArrayList<Aid> readAidFile() throws IOException {
-    // ArrayList<Aid> aidList = new ArrayList<Aid>();
-    // List<String> aids = Files.readAllLines(Paths.get("src/aids.csv"));
-    // System.out.println("Array:");
-    // for (String s : aids)
-    // System.out.println(s);
-    // for (int i = 0; i < aids.size(); i++) {
-    // String[] items = aids.get(i).split(",");
-    // aidList.add(new Aid(items[0], items[1], items[2],
-    // Integer.parseInt(items[3])));
-    // }
-    // return aidList;
-    // }
-
-    // public static void writeAidFile(ArrayList<Aid> aid)
-    // throws IOException {
-    // // read students.csv into a list of lines.
-    // StringBuilder sb = new StringBuilder();
-    // for (int i = 0; i < aid.size(); i++)
-    // sb.append(aid.get(i).toCSVString() + "\n");
-    // Files.write(Paths.get("src/aids.csv"), sb.toString().getBytes());
-    // }
-
 }
--- src/AidsCompleted.java
+++ src/AidsCompleted.java
@@ -6,6 +6,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is create an object of an officially donated item to be stored
+ * together as an object which combines data fields from different classes.
+ */
 public class AidsCompleted {
     public String donor;
     public String phoneNumber;
@@ -15,6 +19,17 @@ public class AidsCompleted {
     public String manpower;
     public String status;
 
+    /**
+     * Constructs an aid object to later be stored in aidsCompleted.csv file.
+     * 
+     * @param donor       Donor's username
+     * @param phoneNumber Donor's phone number
+     * @param aid         Aid item title
+     * @param ngoName     NGO's initials
+     * @param quantity    Aid item quantity
+     * @param manpower    NGO's manpower number
+     * @param status      Aid status (Available, Reserved, or Collected)
+     */
     public AidsCompleted(String donor, String phoneNumber, String aid, String ngoName, int quantity, String manpower,
             String status) {
         this.donor = donor;
@@ -26,61 +41,129 @@ public class AidsCompleted {
         this.status = status;
     }
 
+    /**
+     * Gets status of the aid object.
+     * 
+     * @return Returns either Available, Reserved, or Collected.
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Gets item donor's username.
+     * 
+     * @return Returns the item donor's username.
+     */
     public String getDonor() {
         return donor;
     }
 
+    /**
+     * Gets the phone number of the donor of item.
+     * 
+     * @return Returns the phone number of the item donor.
+     */
     public String getPhone() {
         return phoneNumber;
     }
 
+    /**
+     * Gets the donated item's title.
+     * 
+     * @return Returns aid value.
+     */
     public String getAid() {
         return aid;
     }
 
+    /**
+     * Gets quantity of donated aid item.
+     * 
+     * @return Returns the quantity of the donated aid item.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Gets the NGO's name assigned to the donated item.
+     * 
+     * @return Returns the NGO's name assigned to the donated item.
+     */
     public String getngoName() {
         return ngoName;
     }
 
+    /**
+     * Gets the NGO's manpower.
+     * 
+     * @return Returns the NGO's manpower value.
+     */
     public String getManpower() {
         return manpower;
     }
 
+    /**
+     * Sets the manpower number of the NGO to the AidsCompleted object.
+     * 
+     * @param manpower Manpower number of an NGO
+     */
     public void setManpower(String manpower) {
         this.manpower = manpower;
     }
 
+    /**
+     * Sets the NGO name to the assigned NGO's initials.
+     * 
+     * @param ngoName NGO name assigned to the donated item
+     */
     public void setngoName(String ngoName) {
         this.ngoName = ngoName;
     }
 
+    /**
+     * Sets quantity of donated item.
+     * 
+     * @param quantity Donated aid item quantity
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Sets the status of the donated item.
+     * 
+     * @param status Status of "Available", "Reserved", or "Collected"
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Converts object values to a string that follows a tabular format.
+     */
     public String toString() {
-        // donor, phone, item, quantity, to ngo, manpower, status
         return String.format("%10s\t%12s\t%10s\t%8d\t%7s\t%8s\t%10s",
                 getDonor(), getPhone(), getAid(), getQuantity(), getngoName(), getManpower(), getStatus());
     }
 
+    /**
+     * Creates a string of donated item's details to be stored in the data file.
+     * 
+     * @return Returns a suitable string for writing into the CSV file.
+     */
     public String toCSVString() {
         return getDonor() + "," + getPhone() + "," + getAid() + "," + getngoName() + "," + getQuantity() + ","
                 + getManpower() + "," + getStatus();
     }
 
+    /**
+     * Reads the aidsCompleted.csv file to store into list of all donations made.
+     * 
+     * @return Returns list of donations made to the system.
+     * @throws IOException
+     */
     public static ArrayList<AidsCompleted> readAidsCompletedFile() throws IOException {
         ArrayList<AidsCompleted> aidsCompletedList = new ArrayList<AidsCompleted>();
         List<String> completed = Files.readAllLines(Paths.get("src/aidsCompleted.csv"));
@@ -95,6 +178,12 @@ public class AidsCompleted {
         return aidsCompletedList;
     }
 
+    /**
+     * Writes the donation details onto the aidsCompleted.csv.
+     * 
+     * @param aid AidsCompleted object having details of the donated item.
+     * @throws IOException
+     */
     public static void writeAidsCompletedFile(ArrayList<AidsCompleted> aid)
             throws IOException {
         StringBuilder sb = new StringBuilder();
